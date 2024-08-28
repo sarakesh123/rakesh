fa8521b65c4e99654c80e87ddd63ac43fc092d40
sarakesh123 <sahoorakesh0611@gmail.com>
2024-08-28 12:49:31 +0530

    Add files via upload

---
+++ Assignment_2/r2.cpp
@@ -0,0 +1,3 @@
+/*The statement typedef int (&rifii)(int, int); defines rifii as an alias for a reference to a function that takes two int parameters and returns an int.
+
+this alias makes it easier to refer to functions of this type.It is useful for simplifying code, especially when passing or sorting function references, and improves readability by avoiding repetitive function signatures. For example, you can use rifil to create a reference to a function and call it using a simpler name.*/
